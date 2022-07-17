e2b7587846ddb699860773126501edb990d5c9a2
MSI\User <kong28769@gmail.com>
2022-07-18 00:51:54 +0700

    [Edited] : เพิ่ม async & await

--- src/Moddata.js
+++ src/Moddata.js
@@ -3,7 +3,29 @@ import React, { useEffect, useState } from "react";
 export default function Moddata() {
   const [data, setData] = useState([]);
   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let result = [];
+  let dataObj = [];
+  const [obj, setObj] = useState([
+    {
+      id: 1,
+      name: "kong",
+    },
+    { id: 2, name: "rang" },
+  ]);
+  for (let i = 0; i < number.length; i++) {
+    result.push(number[i] * 2);
+  }
 
+  const pushNumber = () => {
+    let result = [];
+    let total = number.map((item, index) => {
+      result.push(item);
+    });
+  };
+
+  useEffect(() => {
+    pushNumber();
+  }, []);
 
   useEffect(() => {
     let mod = number.map((item) => {
@@ -25,6 +47,29 @@ export default function Moddata() {
       );
     });
   };
+  async function main() {
+    await taskOne();
+    await taskTwo();
+    await taskThree();
+  };
+
+ 
+  function  taskOne () {
+   setTimeout(function () {
+      console.log("this is task 1");
+    }, 500);
+  };
+  function taskTwo () {
+    console.log("this is task 2");
+  };
+  function taskThree () {
+    setTimeout(function () {
+      console.log("this is task 3");
+    }, 1000);
+  };
+
+  main()
+
 
   return (
     <div>
